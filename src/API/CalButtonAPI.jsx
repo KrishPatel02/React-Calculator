f1e5c71c31cad4b948c9f687dd6449882d28e000
Krish Patel <krishpatel200508@gmail.com>
2024-05-21 23:20:20 +0530

    Update API

---
+++ src/API/CalButtonAPI.jsx
@@ -0,0 +1,123 @@
+const calButtonAPI = [
+    {
+        val: "7",
+
+        className: "numBtn",
+    },
+
+    {
+        val: "8",
+
+        className: "numBtn",
+    },
+
+    {
+        val: "9",
+
+        className: "numBtn",
+    },
+
+    {
+        val: "*",
+
+        className: "signBtn",
+    },
+
+    {
+        val: "/",
+
+        className: "signBtn",
+    },
+
+    {
+        val: "4",
+
+        className: "numBtn",
+    },
+
+    {
+        val: "5",
+
+        className: "numBtn",
+    },
+
+    {
+        val: "6",
+
+        className: "numBtn",
+    },
+
+    {
+        val: "-",
+
+        className: "signBtn",
+    },
+
+    {
+        val: "(",
+
+        className: "signBtn",
+    },
+
+    {
+        val: "1",
+
+        className: "numBtn",
+    },
+
+    {
+        val: "2",
+
+        className: "numBtn",
+    },
+
+    {
+        val: "3",
+
+        className: "numBtn",
+    },
+
+    {
+        val: "+",
+        className: "signBtn",
+    },
+
+    {
+        val: ")",
+
+        className: "signBtn",
+    },
+
+    {
+        val: ".",
+
+        className: "signBtn",
+    },
+
+    {
+        val: "0",
+
+        className: "numBtn",
+    },
+    {
+        val: "%",
+
+        className: "signBtn",
+    },
+
+    {
+        val: "DEL",
+
+        className: "signBtn",
+    },
+
+    {
+        val: "=",
+
+        variant: "contained",
+
+        className: "mainBtn",
+    },
+];
+
+export default calButtonAPI;
